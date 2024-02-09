c52266b14223e65f1f73eec4dfb0135e85a6a8d0
Jascha <jascha3600@gmail.com>
2024-02-09 15:19:40 +0100

    Added IOrderState

---
+++ Domain/OrderStates/IOrderState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.OrderStates
+{
+    public interface IOrderState
+    {
+        public string Submit();
+        public void AddReservation(MovieTicket ticket);
+        public int CalculatePrice(List<MovieTicket> tickets);
+        public void Export();
+
+    }
+}
