69f8d2877ab079bcf3b0709bd48d6eaba075017d
xin <xin20wang@outlook.com>
2024-02-09 16:13:37 +0100

    Added paid and canceled state

---
+++ Domain/OrderStates/CanceledState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.OrderStates
+{
+    public class CanceledState : IOrderState
+    {
+        public string Cancel(Order order)
+        {
+            return "Order has been canceled";
+        }
+
+        public string Change(Order order)
+        {
+            return "Canceled order cannot be changed.";
+        }
+
+        public string Pay(Order order)
+        {
+            return "Canceled orders cannot be paid.";
+        }
+
+        public string Reserve(Order order)
+        {
+            return "Order has been reserved";
+        }
+    }
+}
---
+++ Domain/OrderStates/PaidState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.OrderStates
+{
+    public class PaidState : IOrderState
+    {
+        public string Cancel(Order order)
+        {
+            return "Paid order cannot be canceled";
+        }
+
+        public string Change(Order order)
+        {
+            return "Paid order cannot be changed.";
+        }
+
+        public string Pay(Order order)
+        {
+            return "Paid orders is already paid.";
+        }
+
+        public string Reserve(Order order)
+        {
+            return "Paid order has already been reserved";
+        }
+    }
+}
