c97ef0e0d982e865584e20c67a27f3388835f810
Jascha <jascha3600@gmail.com>
2024-02-16 10:13:45 +0100

    Added 12 hour IsMoreThan12HoursBeforeScreening method

--- SOA3_Xin_Jascha/Program.cs
+++ SOA3_Xin_Jascha/Program.cs
@@ -7,14 +7,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            // Export format
-            Order Test = new Order(1, new StudentCustomer());
-            var Movie = new Movie("Steamboat Willie");
-            var Screening = new MovieScreening(Movie, DateTime.Now, 10.50);
-            var Ticket = new MovieTicket(Screening, false, 1, 1);
-            Test.AddSeatReservation(Ticket);
-            Test.Export(TicketExportFormat.JSON);
+            //Console.WriteLine("Hello, World!");
+            //// Export format
+            //Order Test = new Order(1, new StudentCustomer());
+            //var Movie = new Movie("Steamboat Willie");
+            //var Screening = new MovieScreening(Movie, DateTime.Now, 10.50);
+            //var Ticket = new MovieTicket(Screening, false, 1, 1);
+            //Test.AddSeatReservation(Ticket);
+            //Test.Export(TicketExportFormat.JSON);
+
+            DateTime test = DateTime.Now;
+            test = test.AddHours(12);
+            test.AddMinutes(1);
+
 
         }
     }
