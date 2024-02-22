fc6d4fb9928d626e6fff5632bc92738d3984197c
Xin Wang <xx.wang@student.avans.nl>
2024-02-22 23:58:37 +0100

    Created classes for observer patterns

---
+++ Domain/ObservePattern/CanceledSubscriber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Observers
+{
+    public class CanceledSubscriber : ISubscriber
+    {
+        public CanceledSubscriber(Order order) : base(order)
+        {
+        }
+
+        public override void Notify()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domain/OrderObservable.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Observers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public abstract class OrderObservable
+    {
+        private List<ISubscriber> SubscriberCount { get; set; } = new List<ISubscriber>();
+
+        public OrderObservable()
+        {
+            SubscriberCount = new List<ISubscriber>();
+        }
+
+        public void NotifyAll()
+        {
+            foreach (var subscriber in SubscriberCount)
+            {
+                subscriber.Notify();
+            }
+        }
+
+        void Subscribe(ISubscriber subscriber)
+        {
+            this.SubscriberCount.Add(subscriber);
+        }
+
+        void Unsubscribe(ISubscriber subscriber)
+        {
+            this.SubscriberCount.Remove(subscriber);
+        }
+    }
+}
