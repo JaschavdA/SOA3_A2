a6014908cd015ad7cba872986ea9060be6dedb89
Jascha <jascha3600@gmail.com>
2024-02-25 10:30:05 +0100

    Added SMS adapter

---
+++ Domain/Adapter/SMSAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.SendClients;
+
+namespace Domain.Adapter
+{
+    public class SMSAdapter : IAdapter
+    {
+        private readonly TestSMSClient client = new TestSMSClient();
+        public void Send(string message)
+        {
+            this.client.Send(message);
+        }
+    }
+}
---
+++ Domain/SendClients/TestSMSClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.SendClients
+{
+    internal class TestSMSClient
+    {
+        public void Send(String message)
+        {
+            Console.WriteLine("This is an SMS: " + message);
+        }
+    }
+}
